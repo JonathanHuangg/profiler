4d20e18af5791bc07f3b156bc5787c7a2fbda7b8
Jonathan Huang <jonathannhuang@gmail.com>
2026-05-21 00:25:01 +0800

    more linked lists

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -1,5 +1,13 @@
 /*
 No heap allocation becasue of memory fragmentation and cache misses
+
+lvalue is  on the heap. they have an address so you use &
+lvalue needs to be deep copied
+Rvalue is on the stack or in registers so no memory address. 
+rvalue is pointer stealing (move)
+&& is a rvalue reference
+
+rule of 5: destructor, copy constructor, copy assignment operator, move constructor, move assignment operator
 */
 
 template <typename T> 
@@ -9,11 +17,146 @@ class SinglyLinkedList_bare {
         struct Node {
             T data; 
             Node* next; 
-
+            
+            // using template U makes compiler to a type deduction at runtime to see lvalue or rvalue
             template <typename U>
+            // passing l values does a deep copy, passing r value is a move operation
+            // Node(T val, Node* nxt = nullptr) : data(std::move(val)), next(nxt) {}
+            // Node(const T& val, Node* nxt = nullptr) : data(val), next(nxt) {}
             Node(U&& val, Node* nxt=nullptr)
                 : data(std::forward<U>(val)), next(nxt) {}
+        };
+
+        Node* head;
+        std::size_t list_size;
+    
+    public:
+        SinglyLinkedList_bare() : head(nullptr), list_size(0) {}
+
+        ~SinglyLinkedList_bare() { clear(); }
+        
+        // get rid of original copy and copy operator
+        SinglyLinkedList_bare(const SinglyLinkedList_bare&) = delete; 
+        SinglyLinkedList_bare& operator = (const SinglyLinkedList_bare&) = delete;
+
+        // enable shallow copy. noexcept guarantees it never throws exception
+        SinglyLinkedList_bare(SinglyLinkedList_bare && other) noexcept 
+            : head(other.head), list_size(other.list_size) {
+                other.head = nullptr;
+                other.list_size = 0;
         }
+
+        SinglyLinkedList_bare& operator = (SinglyLinkedList_bare&& other) noexcept {
+            if (this != &other) {
+                clear();
+                head = other.head;
+                list_size = other.list_size;
+                other.head = nullptr;
+                other.list_size = 0;
+            }
+            return *this;
+        }
+
+        void clear() {
+            Node* probe = head;
+            while (probe != nullptr) {
+                Node* next = probe->next;
+                delete probe;
+                probe = next;
+            }
+
+            head = nullptr;
+            list_size = 0;
+        }
+
+        // emplace takes raw arguments and builds direcly inside constructor
+        // push takes an already created object
+        template <typename... Args>
+        void emplace_front(Args&&... args) {
+            Node* new_node = new Node(std::forward<Args>(args)...);
+            new_node->next = head;
+            head = new_node;
+            ++list_size;
+        }   
+        /*
+        std::string test = "hello";
+        list.push_front(test);
+        
+        1) because it is a string, lvalue -> template U is std::string&
+        2) push_front(std::string&) -> emplace_front(test)
+        3) in emplace, args is an lvalue reference
+        4) Node constructor runs using std::string copy constructor
+
+        list.push_front(std::string("hello world"));
+        1) this is now a rvalue so U is std::string
+        2) push_front(std::string&& val)
+        3) in emplace, this rvalues must be pointer stolen/moved -> emplace_front(std::move(val))
+        4) Args sees this as std::string
+        5) new Node() is called and because it is rvalue, std::forward casts ravlue std::string into rvalue reference &&std::string&&
+        6) Node constructor runs
+        */
+        void push_front(U&& val) {
+            emplace_front(std::forward<U>(val));
+        }
+
+        void pop_front() {
+            if (head == nullptr) {
+                return;
+            }
+
+            Node* old_head = head;
+            head = head.next;
+            delete old_head;
+            --list_size--;
+        }
+
+        std::size_t size() const {return list_size;}
+        bool empty() const {return list_size == 0;}
+};
+
+template <typename T> 
+class SinglyLinkedList_norm {
+    private:
+        struct Node {
+            T data;
+            Node* next;
+
+            // no direct initialization
+            Node(T val, Node*nxt=nullptr) {
+                data = val;
+                next = nxt; 
+            }
+        };
+        
+        Node* head; 
+        std::size_t list_size;
+    
+    public:
+        // constructor
+        SinglyLinkedList_norm() : head(nullptr), list_size(0) {}
+
+        ~SinglyLinkedList_norm() {clear();}
+
+        // get rid of copy semantics
+        SinglyLinkedList_norm(const SinglyLinkedList_norm&) = delete;
+        SinglyLinkedList_norm& operator = (const SinglyLinkedList_norm&) = delete;
+        
+        void clear() {
+            Node* probe = head;
+
+            while (probe != nullptr) {
+                Node* next_node = probe->next;
+                delete probe;
+                probe = next_node;
+            }
+            head = nullptr;
+            list_size = 0;
+        }
+
+        // move semantis
+        
+
+
 }
 
 class DoublyLinkedList {
